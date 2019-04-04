3ab6969e657250aa2c9143d32b8148fc1acae6b0
Puyan Lotfi <plotfi@fb.com>
2019-04-04 15:09:50 -0700

    [clang-ifso] Fixing up Triple -> Machine name

--- clang/tools/clang-ifso/Symbols2Yaml.cpp
+++ clang/tools/clang-ifso/Symbols2Yaml.cpp
@@ -12,12 +12,26 @@ void Symbols2Yaml(const llvm::Triple &T,
   llvm::StringRef MachineType =
       llvm::StringSwitch<llvm::StringRef>(T.getArchName())
           .Case("x86_64", "EM_X86_64")
-          .Case("x86", "EM_386")
           .Case("i386", "EM_386")
           .Case("i686", "EM_386")
           .Case("aarch64", "EM_AARCH64")
+          .Case("amdgcn", "EM_AMDGPU")
+          .Case("r600", "EM_AMDGPU")
           .Case("arm", "EM_ARM")
-          .Default("EM_X86_64");
+          .Case("thumb", "EM_ARM")
+          .Case("avr", "EM_AVR")
+          .Case("mips", "EM_MIPS")
+          .Case("mipsel", "EM_MIPS")
+          .Case("mips64", "EM_MIPS")
+          .Case("mips64el", "EM_MIPS")
+          .Case("msp430", "EM_MSP430")
+          .Case("ppc", "EM_PPC")
+          .Case("ppc64", "EM_PPC64")
+          .Case("ppc64le", "EM_PPC64")
+          .Case("x86", T.isOSIAMCU() ? "EM_IAMCU" : "EM_386")
+          .Case("x86_64", "EM_X86_64")
+          .Default("EM_NONE");
+
   OS << "--- !ELF\n";
   OS << "FileHeader:\n";
   OS << "  Class:           ELFCLASS";
